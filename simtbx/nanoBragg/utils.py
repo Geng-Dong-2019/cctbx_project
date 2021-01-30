7ec9e19416955dce303c76889cc7416516e7ec77
Derek Mendez <dermen@lbl.gov>
2021-01-29 18:47:09 -0600

    diffBragg code

--- simtbx/nanoBragg/utils.py
+++ simtbx/nanoBragg/utils.py
@@ -8,8 +8,20 @@ from simtbx.nanoBragg import nanoBragg
 from simtbx.nanoBragg.nanoBragg_beam import NBbeam
 from simtbx.nanoBragg.nanoBragg_crystal import NBcrystal
 from simtbx.nanoBragg.sim_data import SimData
+from scitbx.matrix import sqr
 
 ENERGY_CONV = 10000000000.0 * constants.c * constants.h / constants.electron_volt
+
+
+def ensure_p1(Crystal, Famp):
+  high_symm_symbol = Famp.space_group_info().type().lookup_symbol()
+  cb_op = Famp.space_group_info().change_of_basis_op_to_primitive_setting()
+  dtrm = sqr(cb_op.c().r().as_double()).determinant()
+  if not dtrm == 1:
+    Crystal = Crystal.change_basis(cb_op)
+    Famp = Famp.change_basis(cb_op)
+  return Crystal, Famp
+
 
 def flexBeam_sim_colors(CRYSTAL, DETECTOR, BEAM, Famp, energies, fluxes,
                         pids=None, cuda=False, oversample=0, Ncells_abc=(50, 50, 50),
@@ -59,6 +71,8 @@ def flexBeam_sim_colors(CRYSTAL, DETECTOR, BEAM, Famp, energies, fluxes,
   :param mosaicity_random_seeds: random seeds to simulating mosaic texture
   :return: list of [(panel_id0,simulated pattern0), (panel_id1, simulated_pattern1), ...]
   """
+
+  CRYSTAL, Famp = ensure_p1(CRYSTAL, Famp)
 
   if pids is None:
     pids = range(len(DETECTOR))
